6d1954bcaba156a91b3185fa8d89b79ad871f1d2
Nick Garlington <nickgarlington@Nicks-MacBook-Pro-2.local>
2016-03-18 15:17:51 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+var batman;
+var superman;
+var aquaman;
+var deadpool;
+var wonderWoman;
+
+
+var batman = 5;
+var superman = 6;
+var aquaman = 7;
+var deadpool = 8;
+var wonderWoman = 9;
+
+
+var batman = "awesome";
+var superman = "meh";
+var aquaman = "sweet";
+var deadpool = "wicked";
+var wonderWoman = "sexy";
+
+
+
+var batman = true;
+var superman = false;
+var aquaman = true;
+var deadpool = true;
+var wonderWoman = false;
+
+
+var batman = 5;
+var superman = 6;
+var deadpool = batman + superman;
+
+
+var gameOver = true;
+if (gameOver) {
+  console.log("everyone dies a bloody death");
+}
+else {
+  console.log("nobody dies a bloody death");
+}
+
+for (var i = 0; i < 10; i++) {
+
+}
